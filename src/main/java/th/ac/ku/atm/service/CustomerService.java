7f2c7b891b5245de3564dbd6226bd57e56cdd496
bell <weerin.n@ku.th>
2020-09-29 17:52:23 +0700

    atm web jdbc update

--- src/main/java/th/ac/ku/atm/service/CustomerService.java
+++ src/main/java/th/ac/ku/atm/service/CustomerService.java
@@ -1,6 +1,8 @@
 package th.ac.ku.atm.service;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+import th.ac.ku.atm.data.CustomerRepository;
 import th.ac.ku.atm.model.Customer;
 import org.mindrot.jbcrypt.BCrypt;
 import javax.annotation.PostConstruct;
@@ -11,35 +13,41 @@ import java.util.List;
 @Service
 public class CustomerService {
 
-    private List<Customer> customerList;
-    @PostConstruct
-    public void postConstruct(){
-        this.customerList = new ArrayList<>();
+//    private List<Customer> customerList;
+    private CustomerRepository repository;
+
+    public CustomerService(CustomerRepository repository) {
+        this.repository = repository;
     }
+
+    @PostConstruct
+//    public void postConstruct(){
+//        this.customerList = new ArrayList<>();
+//    }
 
     public void  createCustomer(Customer customer){
         //... hash pin for customer ...
         String hashPin = hash(customer.getPin());
         customer.setPin(hashPin);
-        customerList.add(customer);
+        repository.save(customer);
     }
     public List<Customer> getCustomers() {
-        return new ArrayList<>(this.customerList);
+        return repository.findAll();
     }
 
     private String hash(String pin) {
         String salt = BCrypt.gensalt(12);
         return BCrypt.hashpw(pin, salt);
     }
-    public Customer findCustomer(int id){
-        for(Customer customer : customerList){
-            if(customer.getId() == id){
-                return customer;
-            }
-        }
-        return null;
-    }
 
+    public Customer findCustomer(int id){
+        try {
+            return repository.findById(id);
+        } catch (EmptyResultDataAccessException e) {
+            return null;
+        }
+
+    }
     public Customer checkPin(Customer inputCustomer){
         Customer storedCustomer = findCustomer(inputCustomer.getId());
         if(storedCustomer != null){
@@ -51,6 +59,9 @@ public class CustomerService {
         }
         return null;
     }
+
+
+
 
 
 }
